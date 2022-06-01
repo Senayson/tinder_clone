06b52f8a20780aef57162e1b72ee73f076c9e207
Senay <senaialema@gmail.com>
2022-06-01 14:07:19 -0400

    create Header.css

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import "./Header.css";
 
 function Header(){
     return(
-        <div>   
+        <div className='header'>   
+            <PersonIcon/>
             <h2>
                 I am a header
             </h2>
+            <ForumIcon/>
         </div>
     )
 }
+
 
 export default Header
