543c819ddc3bd371a4bbd8c3eb42b46879b9cb43
Senay <senaialema@gmail.com>
2022-06-01 14:22:35 -0400

    style profile tinder message icons

--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,9 @@ import "./Header.css";
 function Header(){
     return(
         <div className='header'>   
-            <PersonIcon/>
-            <h2>
-                I am a header
-            </h2>
-            <ForumIcon/>
+            <PersonIcon className="header_icon" fontSize="large"/>
+            <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon-3.png" alt="tinder logo" />
+            <ForumIcon className="header_icon" fontSize="large"/>
         </div>
     )
 }
